269e93055ff67b50558fa560f06575a25c1418a1
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-25 11:38:08 +0300

    try

--- header.h
+++ header.h
@@ -10,3 +10,22 @@ redisContext* connectToServer(char* IP, int port);
 void flipper() {
     printf("hello world\n");
 }
+
+redisContext* connectToServer(char* IP, int port) {
+    redisContext* context;
+    context = redisConnect(IP, port);
+    /* localhost is used since it's phase I */
+    if (context ==  NULL || context->err) { /* handle unsuccessful connection, taken from github C client */
+        if (context) {
+            printf("Error: %s\n",context->errstr);
+            /* TODO: handle error */
+            /* options: run function again, abort and exit program */
+            return NULL;
+        }
+        else {
+            printf("Can't allocate Redis context\n"); /* TODO: decide what should be written here*/
+            return NULL;
+        }
+    }
+    return context;
+}
