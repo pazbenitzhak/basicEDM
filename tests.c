71fe3b2b8a0685dde1bb489cf1486c3582b0eb84
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 10:20:52 +0300

    add print

--- tests.c
+++ tests.c
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
     port = 6400;
     printf("In tests\n");
     context = connectToServer("127.0.0.1",port); /* connect to right port */
+    printf("after connection\n");
     if (context) { /* context = NULL: exit the program */
         exit(1); /* exit with error */
     }
