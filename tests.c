fe25c31819d6b9596112e66cf81bc95aa252d7a7
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 11:11:06 +0300

    add context related prints

--- tests.c
+++ tests.c
@@ -7,6 +7,8 @@ int main(int argc, char** argv) {
     printf("In tests\n");
     context = connectToServer("127.0.0.1",port); /* connect to right port */
     printf("after connection\n");
+    printf("context error val is: %s\n",context->err);
+    printf("context addrlen is: %li\n",context->addrlen);
     if (context) { /* context = NULL: exit the program */
         printf("if confirmed\n");
         exit(1); /* exit with error */
