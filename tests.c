9e5e589dce74cd0c86c4ca69cd61a3a22229a6ed
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 11:14:22 +0300

    erase prints

--- tests.c
+++ tests.c
@@ -4,14 +4,8 @@ int main(int argc, char** argv) {
     int port;
     redisContext* context;
     port = 6400;
-    printf("In tests\n");
     context = connectToServer("127.0.0.1",port); /* connect to right port */
-    printf("after connection\n");
-    printf("context error str is: %s\n",context->errstr);
-    printf("context err val is: %i\n",context->err);
-    printf("context addrlen is: %li\n",context->addrlen);
     if (context==NULL) { /* context = NULL: exit the program */
-        printf("if confirmed\n");
         exit(1); /* exit with error */
     }
     printf("after if condition\n");
