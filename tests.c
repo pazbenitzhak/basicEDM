ced8fc22099f51a6f08a33d3432802060ca5c5f2
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 11:36:59 +0300

    add data assignment i;2Dn tests

--- tests.c
+++ tests.c
@@ -3,11 +3,13 @@
 int main(int argc, char** argv) {
     int port;
     redisContext* context;
+    char* data;
     port = 6400;
     context = connectToServer("127.0.0.1",port); /* connect to right port */
     if (context==NULL) { /* context = NULL: exit the program */
         exit(1); /* exit with error */
     }
     printf("after if condition\n");
-    getValue("a",context);
+    data = getValue("a",context);
+    printf("data: %s\n",data);
 }
