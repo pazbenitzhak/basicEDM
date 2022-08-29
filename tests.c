f85598cb90e8056f78f4c7c60261102027f5d086
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 16:56:59 +0300

    add another print to tests

--- tests.c
+++ tests.c
@@ -4,12 +4,14 @@ int main(int argc, char** argv) {
     int port;
     redisContext* context;
     char* data;
+    char* data2;
     port = 6400;
     context = connectToServer("127.0.0.1",port); /* connect to right port */
     if (context==NULL) { /* context = NULL: exit the program */
         exit(1); /* exit with error */
     }
-    printf("after if condition\n");
     data = getValue("a",context);
     printf("data: %s\n",data);
+    data2 = getValue("doremi",context);
+    printf("data2: %s\n",data2);
 }
