c55457f455b230f74eed762bf9c5be008f1edb35
pazbenitzhak <guypazbenitzhak@gmail.com>
2022-08-29 10:11:03 +0300

    add print

--- tests.c
+++ tests.c
@@ -4,6 +4,7 @@ int main(int argc, char** argv) {
     int port;
     redisContext* context;
     port = 6400;
+    printf("In tests\n");
     context = connectToServer("127.0.0.1",port); /* connect to right port */
     if (context) { /* context = NULL: exit the program */
         exit(1); /* exit with error */
